958d640628e473bba7ff3e3a13f6a0ec5d45d941
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-06-09 16:13:51 -0500

    fix null player name

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,8 +7,6 @@ var randomNumber = function(min,max) {
 
 var fight = function(enemy) {
     while(enemy.health > 0 && playerInfo.health > 0) {
-                // Alert users that they are starting the round.
-        //window.alert("Welcome to Robot Gladiators");
         
         // Ask user if they'd like to fight or run
         var promptFight = window.prompt(" Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose. ");
@@ -68,8 +66,19 @@ var fight = function(enemy) {
     } 
 };
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is" + name);
+    return name;
+}
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
